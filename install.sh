c6db0a9890e0e31b430f40deeb04925fd8f7f036
AdrianInsua <adrian.insua.y@gmail.com>
2019-06-28 00:09:20 +0200

    [#2] fixed ml corpus split and folder routes

---
+++ install.sh
@@ -0,0 +1,14 @@
+pip install jupyter
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+pip install jupyter_nbextensions_configurator
+jupyter_nbextensions_configurator enable --user
+pip install cufflinks
+pip install tqdm
+pip install lxml
+pip install nltk
+pip install sklearn
+pip install mako
+pip install bs4
+pip install python-levenshtein
+pip install gensim
